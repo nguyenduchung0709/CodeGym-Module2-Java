9fd73682181b5d1ad5da8395e032b2cf15ab97a0
nguyenduchung0709 <15.10a13.hungducnguyen@gmail.com>
2025-05-24 11:10:29 +0700

    lop 2d va 3d

--- inherit/src/class_2d_and3d/Test.java
+++ inherit/src/class_2d_and3d/Test.java
@@ -7,7 +7,7 @@ public class Test {
         Point2D p1 = new Point2D(3.5f,7.8f);
         System.out.println("Array 2D: " + p1);
 
-        Point2D p2 = new Point2D(7.5f,50.8f);
+        Point3D p2 = new Point3D(7.5f,50.8f, 78.4f );
         System.out.println("Array 3D: " + p2);
     }
 }
