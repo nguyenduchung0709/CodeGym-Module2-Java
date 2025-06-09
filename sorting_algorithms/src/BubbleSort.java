57e752f664a919786ba3621d0a155bf75f5cb949
nguyenduchung0709 <15.10a13.hungducnguyen@gmail.com>
2025-06-09 13:50:07 +0700

    sap xep noi bot

---
+++ sorting_algorithms/src/BubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] numbers = {9,6,4,5,4,78,61,3,1,1,2};
+        bubbleSort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void bubbleSort(int[] array) {
+        boolean needNextPass = true;
+        for (int i = 1; i < array.length && needNextPass; i++) {
+            needNextPass = false;
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+
+    }
+}
